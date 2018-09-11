256bd4e42b4b17387cf75f7f1d7755867b48bdb5
Scoti Dodson <scotidodson@gmail.com>
2018-09-11 17:47:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -420,7 +420,8 @@ def get_average_age_for_season(data, season)
         end 
       end 
     end
-    puts array_of_ages.average
+    puts array_of_ages.inject(array.inject(0) { |sum, x| sum += x } / array_of_ages.size.to_f
+	end
         binding.pry 
 end
 get_average_age_for_season(data, "season 16")
